c3a141056fdb8ebbb8093e9aa5fcd3718498377f
Omjee R Giri <omjeergiri@gmail.com>
2026-03-31 20:30:13 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.72%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+        long long l=1,r=x,ans=0;
+        while(l<=r){
+            long long m=l+(r-l)/2;
+            if(m*m<=x){
+                ans=m;
+                l=m+1;
+            }else{
+                r=m-1;
+            }
+        }
+        return ans;
+    }
+};
