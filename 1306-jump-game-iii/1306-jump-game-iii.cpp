ea3750542431233f6f5e78ac08d22f74d6105553
Omjee R Giri <omjeergiri@gmail.com>
2026-05-17 12:46:49 +0530

    Time: 1 ms (54.71%), Space: 35.3 MB (85.6%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n=arr.size();
+        vector<bool> vis(n,false);
+        queue<int> q;
+        q.push(start);
+        vis[start]=true;
+        while(!q.empty()){
+            int i=q.front();q.pop();
+            if(arr[i]==0) return true;
+            if(i+arr[i]<n&&!vis[i+arr[i]]){
+                vis[i+arr[i]]=true;
+                q.push(i+arr[i]);
+            }
+            if(i-arr[i]>=0&&!vis[i-arr[i]]){
+                vis[i-arr[i]]=true;
+                q.push(i-arr[i]);
+            }
+        }
+        return false;
+    }
+};
