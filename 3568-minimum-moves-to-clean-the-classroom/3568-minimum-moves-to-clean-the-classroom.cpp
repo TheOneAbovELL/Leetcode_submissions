5ccfc0a597909afc97f19bcff78e456432865b72
Omjee R Giri <omjeergiri@gmail.com>
2026-09-01 11:19:58 +0530

    Time: 197 ms (88.78%), Space: 91.5 MB (88.78%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int minMoves(vector<string>& g, int e) {
-        int m=g.size(),n=g[0].size(),sr,sc,k=0;
-        int lx[20][20]; memset(lx,-1,sizeof(lx));
-        for(int i=0;i<m;++i){
-            for(int j=0;j<n;++j){
-                if(g[i][j]=='S'){sr=i;sc=j;}
-                else if(g[i][j]=='L') lx[i][j]=k++;
+    int minMoves(vector<string>& a, int e) {
+        int r=a.size(), c=a[0].size(), x, y, l=0;
+        int t[20][20], f[20][20][1024]; 
+        memset(t,-1,sizeof(t)); memset(f,-1,sizeof(f));
+        for(int i=0;i<r;++i){
+            for(int j=0;j<c;++j){
+                if(a[i][j]=='S'){ x=i; y=j; }
+                else if(a[i][j]=='L') t[i][j]=l++;
             }
         }
-        if(!k) return 0;
-        int mx[20][20][1024]; memset(mx,-1,sizeof(mx));
+        if(!l) return 0;
         queue<tuple<int,int,int,int,int>> q;
-        q.push({sr,sc,0,e,0}); mx[sr][sc][0]=e;
+        q.push({x,y,0,e,0}); f[x][y][0]=e;
         int d[4][2]={{-1,0},{1,0},{0,-1},{0,1}};
         while(q.size()){
-            auto [r,c,mask,en,mv]=q.front(); q.pop();
-            if(en==0) continue;
-            for(auto& i:d){
-                int nr=r+i[0],nc=c+i[1];
-                if(nr<0||nr>=m||nc<0||nc>=n||g[nr][nc]=='X') continue;
-                int ne=(g[nr][nc]=='R')?e:en-1,nm=mask;
-                if(lx[nr][nc]!=-1) nm|=(1<<lx[nr][nc]);
-                if(nm==(1<<k)-1) return mv+1;
-                if(ne>mx[nr][nc][nm]){
-                    mx[nr][nc][nm]=ne;
-                    q.push({nr,nc,nm,ne,mv+1});
+            auto [u,v,w,p,s]=q.front(); q.pop();
+            if(p==0) continue;
+            for(auto& h:d){
+                int i=u+h[0], j=v+h[1];
+                if(i<0||i>=r||j<0||j>=c||a[i][j]=='X') continue;
+                int np=(a[i][j]=='R')?e:p-1, nw=w;
+                if(t[i][j]!=-1) nw|=(1<<t[i][j]);
+                if(nw==(1<<l)-1) return s+1;
+                if(np>f[i][j][nw]){
+                    f[i][j][nw]=np;
+                    q.push({i,j,nw,np,s+1});
                 }
             }
         }
